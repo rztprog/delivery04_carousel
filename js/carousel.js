64a40727c6ee617f19be5f0b54c34d3b6fcd0663
rztprog <rztprog@outlook.fr>
2019-04-09 17:10:05 +0200

    Add darkmode

--- js/carousel.js
+++ js/carousel.js
@@ -14,18 +14,32 @@ buttonFor.onclick = nextImg;
 buttonFor.addEventListener("click", () => i++ );
 
 let buttonBac = document.getElementsByClassName("button-img--backward")[0];  
-buttonBac.onclick = prevImg; 
+buttonBac.onclick = nextImg; 
 buttonBac.addEventListener("click", () => i-- );
+
+// let timeout = 500;
+
+
+// Darkmode 
+let y = 1;
+let dark = document.getElementsByClassName("darkmode-button")[0];
+let body = document.getElementsByClassName("body")[0].style;
+dark.onclick = darkmode;
 
 
 ////////// FUNCTION //////////
 //////////////////////////////
 
+// function autoRoll(){
+// 	nextImg;
+// 	timeout = setTimeout('autoRoll();', timeout);
+// }
+
 function nextImg(){
+		if(i > 2){ i = 0; }
+		if(i < 0){ i = 2; }
 
-		if(i >= 3){ i = 0; }
-
-		switch (i) {
+		switch(i) {
 			case 0:
 				lis[i].style.display =  "block";
 				lis[i+1].style.display =  "none";
@@ -36,11 +50,27 @@ function nextImg(){
 				lis[i].style.display =  "block";
 				lis[i+1].style.display = "none";
 				break;
-			case 2:
+			default:
 				lis[i-2].style.display =  "none";
 				lis[i-1].style.display =  "none";
 				lis[i].style.display = "block";
 				break;
 		}
 };
+
+function darkmode(){
+	switch(y) {
+		case 1:
+			body.backgroundColor = "#FFF";
+			body.color = "#000";
+			y--;
+			break;
+		default:
+			body.backgroundColor = "#333";
+			body.color = "#FFF";
+			y++;
+			break;
+	}
+};
+
 
