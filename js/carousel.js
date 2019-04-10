a8915440fe2dfe1a47c730f8e1356cdfbf03daf0
rztprog <rztprog@outlook.fr>
2019-04-10 11:51:31 +0200

    add footer social icon

--- js/carousel.js
+++ js/carousel.js
@@ -8,7 +8,6 @@ let lis = document.getElementsByTagName("li");
 
 // Init the first img to display block
 lis[0].style.display = "block";
-
 let buttonFor = document.getElementsByClassName("button-img--forward")[0];  
 buttonFor.onclick = nextImg; 
 buttonFor.addEventListener("click", () => i++ );
@@ -17,29 +16,36 @@ let buttonBac = document.getElementsByClassName("button-img--backward")[0];
 buttonBac.onclick = nextImg; 
 buttonBac.addEventListener("click", () => i-- );
 
-// let timeout = 500;
-
 
 // Darkmode 
 let y = 1;
 let dark = document.getElementsByClassName("darkmode-button")[0];
+dark.textContent = "☀️";
 let body = document.getElementsByClassName("body")[0].style;
 let header = document.getElementsByClassName("header")[0].style;
+header.backgroundColor = "#444";
 dark.onclick = darkmode;
+
+
+
+
+// AutoRoll
+document.getElementsByClassName("carousel")[0].onload = "autoRoll()";
+// let timeout = 500;
 
 
 ////////// FUNCTION //////////
 //////////////////////////////
 
 // function autoRoll(){
-// 	nextImg;
+// 	nextImg();
 // 	timeout = setTimeout('autoRoll();', timeout);
 // }
 
 function nextImg(){
 		if(i > 2){ i = 0; }
 		if(i < 0){ i = 2; }
-
+		console.log("Image: " + i);
 		switch(i) {
 			case 0:
 				lis[i].style.display =  "block";
@@ -65,12 +71,14 @@ function darkmode(){
 			body.backgroundColor = "#FFF";
 			body.color = "#000";
 			header.backgroundColor = "#DDD";
+			dark.textContent = "🌑";
 			y--;
 			break;
 		default: 	// DARK
 			body.backgroundColor = "#333";
 			body.color = "#FFF";
 			header.backgroundColor = "#444";
+			dark.textContent = "☀️";
 			y++;
 			break;
 	}
